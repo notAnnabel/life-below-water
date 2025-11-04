777178ee28e2b28d2e0219f75c8f621cabe5d83c
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-11-04 13:36:03 +0000

    failsafe

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,12 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import { useEffect } from "react";
-import { s } from "lifebelowwater";
-import p5 from "p5";
+
+
 
 export function meta({}: Route.MetaArgs) {
   useEffect(() => {
-     const myP5 = new p5(s);
+
   },[])
   return [
     { title: "New React Router App" },
