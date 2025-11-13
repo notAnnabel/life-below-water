b4f8de3dd12e3fa392676c0e52f564862507f8ee
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-11-13 16:38:46 +0000

    omg my fish die :DDDDDD

--- app/helpers/lifebelowwater.tsx
+++ app/helpers/lifebelowwater.tsx
@@ -3,12 +3,13 @@ import { getPollution } from "./backend";
 
 
 interface p5props {
+  fish: boolean;
   greenWater: number,
   blueWater: number
 }
 
 function sketch(p5: any) {
-
+  
   let xspacing = 12; // Distance between each horizontal location
   let w; // Width of entire wave
   let theta = 0.0; // Start angle at 0
@@ -16,14 +17,19 @@ function sketch(p5: any) {
   let period = 500.0; // How many pixels before the wave repeats
   let dx: number; // Value for incrementing x
   let yvalues: Array<number>; // Using an array to store height values for the wave
-
+  
   let wavenum; // number of waves
   let g; // green variable in rgb
   let b; // blue variable in rgb
-
+  
   let livingimg: any // fish image test
   let deadimg: any // dead fish image 
-  let fish: boolean // true is alive, false is dead
+  let fish = true; // true is alive, false is dead
+
+  // let fishOne;
+  
+  let sun = p5.color(255, 204, 0);
+ 
 
   let xpos = 200; // fish x position
   let ypos = p5.windowHeight / 2; // fish y position
@@ -32,6 +38,9 @@ function sketch(p5: any) {
   let greenWater = 200;
 
   p5.updateWithProps = (props: p5props) => {
+
+    console.log(props);
+
     if (props.blueWater) {
       blueWater = props.blueWater
     }
@@ -40,11 +49,15 @@ function sketch(p5: any) {
     }
 
     // props for changing the fish to dead
+    if (props.fish != undefined) {
+      fish = props.fish
+    }
   }
 
   p5.preload = () => {
     livingimg = p5.loadImage('/assets/fish-png-1.png');
-    deadimg = p5.loadImage('/assets/fish-png-1-dead.png')
+    deadimg = p5.loadImage('/assets/fish-png-1-dead.png');
+    // fishOne = new Fish('/assets/fish-png-1.png', '/assets/fish-png-1-dead.png', 200, py.windowHeight / 2, p5);
   }
 
   p5.setup = () => {
@@ -52,16 +65,47 @@ function sketch(p5: any) {
     w = p5.width + 16;
     dx = (p5.TWO_PI / period) * xspacing;
     yvalues = new Array(p5.floor(w / xspacing));
-  };
 
+  };
+  
   p5.draw = () => {
-    p5.background(220, 160, 100);
+    p5.background("#99D9EA");
     // p5.background(220, 160, 100);
+    p5.fill(sun);
+    p5.noStroke();
+    p5.circle(25, 25, 160);
+    
+    //p5.textSize(16);
+    //title = p5.text('Life below water', 10, 60);
+    //p5.fill(text)
+    
+    
     calcWave();
     renderWave();
-    p5.image(livingimg, xpos, ypos, 100, 100)
-
+    if (fish===true){
+      p5.image(livingimg, xpos, ypos, 100, 100);
+    }
+    if (fish===false){
+      p5.image(deadimg, xpos, ypos, 100, 100);
+      console.log("dead");
+    }
+    
+    // fish drawing
+    if (xpos < p5.width) {
+      xpos = xpos + 2;
+    }
+    else {
+      xpos = -100;
+      ypos = p5.random(p5.height / 2, p5.width);
+    }
   };
+
+  p5.windowResized = () => {
+  p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+  w = p5.width + 16;
+    dx = (p5.TWO_PI / period) * xspacing;
+    yvalues = new Array(p5.floor(w / xspacing));
+}
 
   function calcWave() {
     // Increment theta (try different values for
@@ -94,14 +138,7 @@ function sketch(p5: any) {
       currentHeight = currentHeight + 20;
     }
 
-    // fish drawing
-    if (xpos < p5.width) {
-      xpos = xpos + 2;
-    }
-    else {
-      xpos = -100;
-      ypos = p5.random(p5.height / 2, p5.width);
-    }
+  
   
 
     //     g = p5.random(180,220);
@@ -115,11 +152,13 @@ function sketch(p5: any) {
 const waterconsts = {
   polluted: {
     blue: 200,
-    green: 150
+    green: 150,
+    fish: false,
   },
   clean: {
     blue: 220,
-    green: 190
+    green: 190,
+    fish: true,
   }
 }
 
@@ -146,7 +185,8 @@ export default function Sketch(props: sketchProps) {
   return <P5Wrapper
     sketch={sketch}
     blueWater={props.pollution ? waterconsts.polluted.blue : waterconsts.clean.blue}
-    greenWater={props.pollution ? waterconsts.polluted.green : waterconsts.clean.green} // ? means if, : is else
+    greenWater={props.pollution ? waterconsts.polluted.green : waterconsts.clean.green} // ? means if, : means else
+    fish={props.pollution ? waterconsts.polluted.fish : waterconsts.clean.fish}
   />;
 }
 
